703c0ac2b3f488b84f3ebe851d7d648897f82ece
unknown <rqin@BIGGIE.psychogenics.com>
2015-12-07 11:00:34 -0500

    add an async exception wrapper

---
+++ AsyncInvoker.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace CSharpCodeSnippets.ExceptionHandling
+{
+    public class AsyncInvoker
+    {
+        private static readonly ILog _log = LogManager.GetLogger(typeof (AsyncInvoker));
+
+
+        public static async Task<Result<T>> Invoke<T>(Func<Task<T>> action, string failMessage = "")
+        {
+            try
+            {
+                T result = await action();
+                return result != null ? Result<T>.Ret(result) : Result<T>.Fail(failMessage);
+            }
+            catch (Exception ex)
+            {
+                _log.Error(failMessage + "\n\n" +ex.Message + "\n\n" + ex.StackTrace);
+                return Result<T>.Fail(ex.Message);
+            }
+        }
+
+        public static async Task<Result<T>> Invoke<T, E>(Func<Task<T>> action, string failMessage = "") where E: Exception
+        {
+            try
+            {
+                T result = await action();
+                return result != null ? Result<T>.Ret(result) : Result<T>.Fail(failMessage);
+            }
+            catch (E spExp)
+            {
+                return
+                    Result<T>.Fail(failMessage);
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex.Message + "\n\n" + ex.StackTrace);
+                return Result<T>.Fail(ex.Message);
+            }
+        }
+
+        // how to use
+        // lets first try as sample 
+        public async Task<string> TestAsyncService()
+        {
+            return await Task.Run(() => "Sample");
+        }
+        
+        // This is how to call it
+        public async Task<Result<string>> TestInvoke()
+        {
+            return await Invoke<string, Exception>(async () => await TestAsyncService(), "Sample Error");
+        }
+    }
+}
---
+++ ExceptionHandling/AsyncInvoker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace CSharpCodeSnippets.ExceptionHandling
+{
+    class AsyncInvoker
+    {
+        private static readonly ILog _log = LogManager.GetLogger(typeof (AsyncInvoker));
+
+        public static async Task<Result<T>> Invoke<T, E>(Func<Task<T>> action, string failMessage = "") where E : Exception
+        {
+            try
+            {
+                T result = await action();
+                return result != null ? Result<T>.Ret(result) : Result<T>.Fail(failMessage);
+            }
+            catch (E ex)
+            {
+                return
+                    Result<T>.Fail(failMessage);
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex.Message + "\n\n" + ex.StackTrace);
+                return Result<T>.Fail(ex.Message);
+            }
+        }
+
+        public static async Task<Result<T>> Invoke<T>(Func<Task<T>> action, string failMessage = "") 
+        {
+            try
+            {
+                T result = await action();
+                return result != null ? Result<T>.Ret(result) : Result<T>.Fail(failMessage);
+            }
+            catch (Exception ex)
+            {
+                _log.Error(failMessage + "\n\n" + ex.Message + "\n\n" + ex.StackTrace);
+                return Result<T>.Fail(ex.Message);
+            }
+        }
+
+        public async Task<string> TestService()
+        {
+            return await Task.Run(() => "Something");
+        }
+
+        public async Task<Result<string>> TestRunExceptionWrapper()
+        {
+            return await Invoke(async () => await TestService(), "There is something I need to tell you");
+        }
+
+    }
+}
